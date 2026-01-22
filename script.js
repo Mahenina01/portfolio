1dffcc40a4fdd45305791d2897949a2ecd282a7e
Mahenina01 <kylleandriamahenina@gmail.com>
2026-01-22 12:41:34 +0300

    First portfolio

---
+++ script.js
@@ -0,0 +1,22 @@
+const index = document.querySelector('.nav-index');
+const items = document.querySelectorAll('li'); 
+
+function handleIndicator(element){
+// boucle sur tous items pour retirer 'is-active'
+
+    items.forEach(item=>{
+        item.classList.remove("is-active");
+    });
+// style for index 
+    index.style.width=`${element.offsetWidth}px`;
+
+    //ajout element 
+    element.style.color='#dc45fe';
+}
+
+items.forEach((item)=>{
+    item.addEventListener('click',(e)=>{
+        handleIndicator(e.target);
+    });
+    item.classList.contains('is-active') && handleIndicator(item);//ternaire avancer pour eviter ? et le if condition
+});
